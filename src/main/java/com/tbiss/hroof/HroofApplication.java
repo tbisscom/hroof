082bfdebf5cf676545d5edf32635f3404bdf6426
Khaled Sayed <contact@khaledsayed.com>
2019-01-07 15:36:07 +0200

    init project

---
+++ src/main/java/com/tbiss/hroof/HroofApplication.java
@@ -0,0 +1,14 @@
+package com.tbiss.hroof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HroofApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HroofApplication.class, args);
+    }
+
+}
+
